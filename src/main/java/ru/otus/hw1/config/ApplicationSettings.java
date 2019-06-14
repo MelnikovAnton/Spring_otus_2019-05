a7023141a136f5185f86de7dd1fc4b3a8eef31a7
Anton Melnikov <melnikoffaa@mail.ru>
2019-06-14 08:33:05 +0300

    Add Lombock to ApplicationSettings

--- src/main/java/ru/otus/hw1/config/ApplicationSettings.java
+++ src/main/java/ru/otus/hw1/config/ApplicationSettings.java
@@ -1,5 +1,6 @@
 package ru.otus.hw1.config;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,43 +8,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "application")
+@Data
 public class ApplicationSettings {
 
     private String answer;
     private String question;
     private String locale;
     private int required;
-
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getLocale() {
-        return locale;
-    }
-
-    public void setLocale(String locale) {
-        this.locale = locale;
-    }
-
-    public int getRequired() {
-        return required;
-    }
-
-    public void setRequired(int required) {
-        this.required = required;
-    }
 }
--- src/main/java/ru/otus/hw1/dao/Dao.java
+++ src/main/java/ru/otus/hw1/dao/Dao.java
@@ -1,12 +1,8 @@
 package ru.otus.hw1.dao;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
-import ru.otus.hw1.config.Config;
 import ru.otus.hw1.utils.DataValidationException;
 
-import java.net.URL;
 import java.util.List;
 
 
